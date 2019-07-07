a547fb14d88b533da748fc3b2bab87120a0f3247
kalyan <24x7kalyan@gmail.com>
2019-07-07 12:26:15 -0400

    added code in the taxi queuing design system

--- Pointers_Structures/taxi_queueing_system_design.cpp
+++ Pointers_Structures/taxi_queueing_system_design.cpp
@@ -6,3 +6,60 @@
 
 #include<iostream>
 using namespace std;
+
+//The structure definition of the entities involved
+struct Driver{string name,int id;};
+struct LinkedTaxi{int id,Driver *drv, LinkedTaxi *next;};
+struct Queue{LinkedTaxi *front,*end;int numTaxis;};
+
+int main()
+{
+Queue q;
+q.front=NULL;
+q.end=NULL;
+q.numTaxis=0;
+
+//present options to the user and get input
+while(true)
+{
+  cout<<"Command: 'j' to join the queue, 'd' to dispatch, 'x' to exit. "<<endl;
+  char choice;
+  cin>>choice;
+  switch(choice)
+  {
+    case 'j':
+
+    Driver *newDrv; newDrv=new Driver;
+    if(newDrv==NULL){cout<<"Memory allocation failure."<<endl;return -1;}
+    cout<<"Enter the name of the driver:"<<endl;
+    cin>>newDrv->name;
+    cout<<"Enter the id of the driver:"<<endl;
+    cin>>newDrv->id
+    LinkedTaxi *newTaxi;newTaxi = new LinkedTaxi;
+    if(newTaxi==NULL){cout<<"Memory allocation failure."<<endl;return -1;}
+    newTaxi->drv=newDrv;newTaxi->next=NULL;
+    cout<<"Enter the id of the taxi:"<<endl;
+    cin>>newTaxi->id;
+    if(q.end==NULL){
+    q.front=newTaxi;q.end=newTaxi;q.numTaxis=1;
+    }
+    else
+    {
+    (q.end)->next=newTaxi;q.end=newTaxi;q.numTaxis++;
+    }
+    break;
+
+    case 'd':
+    /*
+
+    */
+    break;
+
+    case 'x':cout<<"Thank You "<<endl; return 0;
+    default: cout<<"Invalid Command."<<endl;
+
+  }
+
+}
+
+}
