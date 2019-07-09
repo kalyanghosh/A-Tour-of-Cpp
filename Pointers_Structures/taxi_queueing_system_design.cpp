570bbc9ef796d8bdaf27a86ca6df430870fab573
kalyan <24x7kalyan@gmail.com>
2019-07-09 10:37:22 -0400

    added final code of taxi design system

--- Pointers_Structures/taxi_queueing_system_design.cpp
+++ Pointers_Structures/taxi_queueing_system_design.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 //The structure definition of the entities involved
-struct Driver{string name,int id;};
-struct LinkedTaxi{int id,Driver *drv, LinkedTaxi *next;};
+struct Driver{string name;int id;};
+struct LinkedTaxi{int id;Driver *drv;LinkedTaxi *next;};
 struct Queue{LinkedTaxi *front,*end;int numTaxis;};
 
 int main()
@@ -34,7 +34,7 @@ while(true)
     cout<<"Enter the name of the driver:"<<endl;
     cin>>newDrv->name;
     cout<<"Enter the id of the driver:"<<endl;
-    cin>>newDrv->id
+    cin>>newDrv->id;
     LinkedTaxi *newTaxi;newTaxi = new LinkedTaxi;
     if(newTaxi==NULL){cout<<"Memory allocation failure."<<endl;return -1;}
     newTaxi->drv=newDrv;newTaxi->next=NULL;
@@ -50,9 +50,30 @@ while(true)
     break;
 
     case 'd':
-    /*
+    if(q.front==NULL)
+    {
+      cout<<"Sorry! No taxis in the queue at present."<<endl;
+    }
+    else
+    {
+      LinkedTaxi *dispatchTaxi;
+      dispatchTaxi=q.front;
+      if(q.front==q.end){
+        q.front=NULL;q.end==NULL;q.numTaxis=0;
+      }
+      else
+      {
+        q.front=(q.front)->next;
+        q.numTaxis--;
+      }
+      if(dispatchTaxi!=NULL)
+      {
+        cout<<"Dispatching Taxi with Id : "<<dispatchTaxi->id<<endl;
+        if(dispatchTaxi->drv!=NULL){delete dispatchTaxi->drv;}
+        delete dispatchTaxi;
+      }
+    }
 
-    */
     break;
 
     case 'x':cout<<"Thank You "<<endl; return 0;
